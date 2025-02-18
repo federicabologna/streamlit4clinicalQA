dde398a7faf24a3cd495ee83b940ac8c60fb6a07
federicabologna <federica.bologna17@gmail.com>
2025-02-18 13:51:57 -0500

    adding script again

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,6 +136,16 @@ def instructions_page2():
 
 def questions_page3():
     
+    time.sleep(.5)
+    js = '''
+        <script>
+            var body = window.parent.document.querySelector(".main");
+            console.log(body);
+            body.scrollTop = 0;
+        </script>
+        '''
+    st.components.v1.html(js, height=0)
+    
     annotation_d = st.session_state.responses_todo[0]
     annotation_type = annotation_d['annotation_type']
     annotations_collection = st.session_state.annotation_collection
