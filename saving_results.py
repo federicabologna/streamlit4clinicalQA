666720c43b542cc70db9062b16d4d9c07e731ea3
federicabologna <federica.bologna17@gmail.com>
2025-03-28 22:59:31 -0400

    freeing more batches

--- saving_results.py
+++ saving_results.py
@@ -25,7 +25,7 @@ def export_annotations_to_jsonl(mongo_uri, db_name, output_folder):
 
 uri = f"mongodb+srv://{open(os.path.join('..', '..', 'PhD', 'apikeys', 'mongodb_clinicalqa_uri.txt')).read().strip()}/?retryWrites=true&w=majority&appName=clinicalqa"
 client = MongoClient(uri)
-db_name = "pilot"  # Database containing multiple collections
-output_folder = "pilot_results"
-os.makedirs(os.path.join('output', 'pilot_results'), exist_ok=True)
+db_name = "coarse"  # Database containing multiple collections
+output_folder = "coarse"
+os.makedirs(os.path.join('output', output_folder), exist_ok=True)
 export_annotations_to_jsonl(uri, db_name, output_folder)
